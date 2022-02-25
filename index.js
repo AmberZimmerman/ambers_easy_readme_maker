df37df4b145e7943a37d556a819b3bd4174125a4
Amber Zimmerman <ajz1221@gmail.com>
2022-02-25 15:57:29 -0700

    starting array of questions for user to fill out

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+// External packages
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const criteria = () => 
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            default: 'Amber Zimmerman',
+        },
+        {
+            type: 'input',
+            message: 'What is your github user name (no @ required)?',
+            name: 'name',
+            default: 'Amber Zimmerman',
+        }, 
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            default: 'Amber Zimmerman',
+        }, 
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            default: 'Amber Zimmerman',
+        }, 
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            default: 'Amber Zimmerman',
+        }, 
+       
+    ])
